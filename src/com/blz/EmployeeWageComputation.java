805bbba31762d45b9e3dc77a9c3590965649cdde
Priyanka <priyankadombale27@gmail.com>
2022-02-15 01:07:52 +0530

    Added : Solving usng switch case logic

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -5,28 +5,28 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		System.out.println(" Welcome in Employee Wage Computation....!!");
 
-		int randomNum = (int) (Math.random() * 2); // create random number using math function
+		int randomNum = (int) (Math.random() * 3); // create random number using math function
 		int wagePerHour = 20;
 		int empHrs = 0;
 		int empHrsf = 8;
 		int empHrsP = 4;
-		if (randomNum == 1) // Check Employee presence
-		{
-			System.out.println(" Employee is Present ");
-
-			if (empHrsf == 8) {
-				int salary = wagePerHour * empHrsf; // Employee Full timeSalary/Day
-				System.out.println(" Employee salary of Full time is :" + salary);
-			}
-			if (empHrsP == 4) {
-				int salary = wagePerHour * empHrsP; // Employee Part time Salary/Day
-				System.out.println(" Employee salary of Part time is :" + salary);
-			}
-		} else {
+		switch (randomNum) {
+		case 0:
 			System.out.println(" Employee is Absent ");
-			int salary = wagePerHour * empHrs; // Employee No Salary/Day
-			System.out.println(" Employee salary is :" + salary);
+			int salary = wagePerHour * empHrs; // Employee full Salary/Day
+			System.out.println("Employee salary is :" + salary);
+			break;
+		case 1:
+			System.out.println(" Employee is Present full time ");
+			int salaryf = wagePerHour * empHrsf; // Employee full Salary/Day
+			System.out.println("Employee salary is :" + salaryf);
+			break;
+		case 2:
+			System.out.println(" Employee is Present part time ");
+			int salaryp = wagePerHour * empHrsP; // Employee full Salary/Day
+			System.out.println("Employee salary is :" + salaryp);
+			break;
 		}
-	}
 
+	}
 }
