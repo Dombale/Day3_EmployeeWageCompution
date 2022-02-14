11bacb2f09fcc88304de5a6c5e12f3dfff2f622e
Priyanka <priyankadombale27@gmail.com>
2022-02-15 00:13:29 +0530

    Added: Check Employee present and Absent Logic

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -4,5 +4,13 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome in Employee Wage Computation....!!");
+		int randomNum = (int) (Math.random() * 2); // create random number using math function
+
+		if (randomNum == 1) // Check Employee presence
+		{
+			System.out.println(" Employee is Present " + randomNum);
+		} else
+			System.out.println(" Employee is Absent " + randomNum);
 	}
+
 }
