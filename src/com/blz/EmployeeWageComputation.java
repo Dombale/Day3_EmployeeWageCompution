1b0a726262993e85657aa7447073f7810f658c15
Priyanka <priyankadombale27@gmail.com>
2022-02-15 00:53:02 +0530

    Added :Parttime Employee Daily Wage And Hrs logic

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -9,16 +9,23 @@ public class EmployeeWageComputation {
 		int wagePerHour = 20;
 		int empHrs = 0;
 		int empHrsf = 8;
-
+		int empHrsP = 4;
 		if (randomNum == 1) // Check Employee presence
 		{
 			System.out.println(" Employee is Present ");
-			int salary = wagePerHour * empHrsf; // Employee Full timeSalary/Day
-			System.out.println("Employee salary of Full time is :" + salary);
+
+			if (empHrsf == 8) {
+				int salary = wagePerHour * empHrsf; // Employee Full timeSalary/Day
+				System.out.println(" Employee salary of Full time is :" + salary);
+			}
+			if (empHrsP == 4) {
+				int salary = wagePerHour * empHrsP; // Employee Part time Salary/Day
+				System.out.println(" Employee salary of Part time is :" + salary);
+			}
 		} else {
 			System.out.println(" Employee is Absent ");
-			int salary = wagePerHour * empHrs; // No Working , No Salary
-			System.out.println("Employee salary is :" + salary);
+			int salary = wagePerHour * empHrs; // Employee No Salary/Day
+			System.out.println(" Employee salary is :" + salary);
 		}
 	}
 
