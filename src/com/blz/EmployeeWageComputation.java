a46d7d3c6021b9d4ed098fd786209fc2aa47e1a3
Priyanka <priyankadombale27@gmail.com>
2022-02-15 00:45:10 +0530

    Added : Calculate Employee Daily Wage logic for full day

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -4,13 +4,22 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome in Employee Wage Computation....!!");
+
 		int randomNum = (int) (Math.random() * 2); // create random number using math function
+		int wagePerHour = 20;
+		int empHrs = 0;
+		int empHrsf = 8;
 
 		if (randomNum == 1) // Check Employee presence
 		{
-			System.out.println(" Employee is Present " + randomNum);
-		} else
-			System.out.println(" Employee is Absent " + randomNum);
+			System.out.println(" Employee is Present ");
+			int salary = wagePerHour * empHrsf; // Employee Full timeSalary/Day
+			System.out.println("Employee salary of Full time is :" + salary);
+		} else {
+			System.out.println(" Employee is Absent ");
+			int salary = wagePerHour * empHrs; // No Working , No Salary
+			System.out.println("Employee salary is :" + salary);
+		}
 	}
 
 }
